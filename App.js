3fcf9d4475febfe2e4a2ca784b16b2a5ea6eec21
arisupriatna14 <arisupriatna703@gmail.com>
2018-09-20 19:33:40 +0700

    Bug fixing camera

--- App.js
+++ App.js
@@ -1,42 +1,24 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image} from 'react-native';
-import Camera from './src/components/Camera';
+import CameraPicker from './src/components/CameraPicker';
+import { createStackNavigator } from 'react-navigation'
+
+const RootStack = createStackNavigator({
+  Camera: {
+    screen: CameraPicker
+  }
+})
 
 export default class App extends Component {
   state = {
     imageUri: null
   }
 
-  setImage = (image) => {
-    console.log('dari setImage ==>', image.uri)
-    this.setState({ imageUri: image.uri })
-  }
-
   render() {
-    const { imageUri } = this.state
     return (
       <View>
-        <Camera getPicImage={this.setImage}/>
+        <CameraPicker/>
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
