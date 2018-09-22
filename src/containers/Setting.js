65966e849bb53889008d5e9cbc97bb4b8f40c4ae
arisupriatna14 <arisupriatna703@gmail.com>
2018-09-22 21:11:47 +0700

    add dependencies react-native-modal-datetime-picker

--- src/containers/Setting.js
+++ src/containers/Setting.js
@@ -2,12 +2,63 @@ import React, { Component } from 'react';
 import {Platform, StyleSheet, Text, View, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/SimpleLineIcons'
 import { Container, ListItem, List, Content, Right, Left, DatePicker } from 'native-base'
+import DateTimePicker from 'react-native-modal-datetime-picker'
 
 class Prescription extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = { 
+      isDateTimePickerVisibleMorning: false,
+      isDateTimePickerVisibleEvening: false,
+      isDateTimePickerVisibleNight: false,
+      timeMorning: null,
+      timeEvening: null,
+      timeNight: null
+    };
   }
+
+  _showDateTimePickerMorning = () => {
+    this.setState({
+      isDateTimePickerVisibleMorning: true
+    })
+  }
+
+  _showDateTimePickerEvening = () => {
+    this.setState({
+      isDateTimePickerVisibleEvening: true
+    })
+  }
+
+  _showDateTimePickerNight = () => {
+    this.setState({
+      isDateTimePickerVisibleNight: true
+    })
+  }
+
+  // _hideDateTimePicker = () => {
+  //   this.setState({
+  //     isDateTimePickerVisible: false
+  //   })
+  // }
+
+  _handleTimeMorning = (time) => {
+    const times = time.toLocaleTimeString('it-IT')
+    this.setState({ timeMorning: times, isDateTimePickerVisibleMorning: false })
+    // this._hideDateTimePicker()
+  }
+
+  _handleTimeEvening = (time) => {
+    const times = time.toLocaleTimeString('it-IT')
+    this.setState({ timeEvening: times, isDateTimePickerVisibleEvening: false })
+    // this._hideDateTimePicker()
+  }
+
+  _handleTimeNight = (time) => {
+    const times = time.toLocaleTimeString('it-IT')
+    this.setState({ timeNight: times, isDateTimePickerVisibleNight: false })
+    // this._hideDateTimePicker()
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -25,41 +76,61 @@ class Prescription extends Component {
           <List>
             <ListItem>
               <Left>
-                <Text style={{fontSize: 18}}>Pagi</Text>
+                <Text style={{fontSize: 18}}>Morning</Text>
               </Left>
               <Right>
-                <DatePicker
-                  defaultDate={new Date()}
-                  minimumDate={new Date(1600, 1, 1)}
-                  maximumDate={new Date(2018, 12, 31)}
-                  locale={"id"}
-                  timeZoneOffsetInMinutes={undefined}
-                  modalTransparent={false}
-                  animationType={"fade"}
-                  androidMode={"default"}
-                  placeHolderText="Birth Date"
-                  textStyle={{ color: "black" }}
-                  placeHolderTextStyle={{ color: "black" }}
-                  onDateChange={this.setDate}
+              <TouchableOpacity onPress={this._showDateTimePickerMorning}>
+                {
+                  this.state.timeMorning ? <Text>{this.state.timeMorning}</Text> : <Text>Set Times</Text>
+                }
+              </TouchableOpacity>
+              <DateTimePicker
+                mode="time"
+                isVisible={this.state.isDateTimePickerVisibleMorning}
+                is24Hour={true}
+                onConfirm={this._handleTimeMorning}
+                onCancel={this._hideDateTimePicker}
+              />
+              </Right>
+            </ListItem>
+
+            <ListItem>
+              <Left>
+                <Text style={{fontSize: 18}}>Evening</Text>
+              </Left>
+              <Right>
+                <TouchableOpacity onPress={this._showDateTimePickerEvening}>
+                  {
+                    this.state.timeEvening ? <Text>{this.state.timeEvening}</Text> : <Text>Set Times</Text>
+                  }
+                </TouchableOpacity>
+                <DateTimePicker
+                  mode="time"
+                  isVisible={this.state.isDateTimePickerVisibleEvening}
+                  is24Hour={true}
+                  onConfirm={this._handleTimeEvening}
+                  onCancel={this._hideDateTimePicker}
                 />
               </Right>
             </ListItem>
 
             <ListItem>
               <Left>
-                <Text style={{fontSize: 18}}>Siang</Text>
+                <Text style={{fontSize: 18}}>Night</Text>
               </Left>
               <Right>
-                <Text>Setting Times</Text>
-              </Right>
-            </ListItem>
-
-            <ListItem>
-              <Left>
-                <Text style={{fontSize: 18}}>Malam</Text>
-              </Left>
-              <Right>
-                <Text>Setting Times</Text>
+                <TouchableOpacity onPress={this._showDateTimePickerNight}>
+                  {
+                    this.state.timeNight ? <Text>{this.state.timeNight}</Text> : <Text>Set Times</Text>
+                  }
+                </TouchableOpacity>
+                <DateTimePicker
+                  mode="time"
+                  isVisible={this.state.isDateTimePickerVisibleNight}
+                  is24Hour={true}
+                  onConfirm={this._handleTimeNight}
+                  onCancel={this._hideDateTimePicker}
+                />
               </Right>
             </ListItem>
           </List>
