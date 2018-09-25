f63efadff4a752a0157c5bb14a6a81f0f658cd8d
Grayfield <grayfieldmm@gmail.com>
2018-09-26 04:01:20 +0700

    implement socket client for chats

--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 const baseURL = 'http://susi-api.arisupriatna.com/';
 const socketURL = 'http://192.168.100.8:3031';
+=======
+let baseURL = 'http://192.168.100.8:3030/';
+let socketURL = 'http://192.168.100.8:3031/';
+
+>>>>>>> implement socket client for chats
 export { baseURL, socketURL };
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import socketio from 'socket.io-client';
-
 import PushConfig from './PushConfig';
 import PushNotification from 'react-native-push-notification';
-import { socketUrl } from '../config';
+import { socketURL } from '../config';
 
 class Home extends Component {
   constructor(props) {
@@ -20,6 +18,7 @@ class Home extends Component {
   componentDidMount() {
     //ini nih notifnya
 <<<<<<< HEAD
+<<<<<<< HEAD
     PushNotification.cancelAllLocalNotifications()// ini buat matiin notif
     // PushNotification.localNotificationSchedule({
       
@@ -32,6 +31,9 @@ class Home extends Component {
     // });
 =======
     PushNotification.cancelAllLocalNotifications();
+=======
+    // PushNotification.cancelAllLocalNotifications();
+>>>>>>> implement socket client for chats
     PushNotification.localNotificationSchedule({
       message: 'Susi', // isi messagenya disini
       repeatType: 'minute', // set aja mau per apa, year,month, week, day, hour , minute
@@ -69,7 +71,6 @@ class Home extends Component {
         <GiftedChat
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
-          onLongPress={this.try}
           user={{
             _id: 1
           }}
